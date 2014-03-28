03677e1b5fe65c1e1a9502128430ea0eb765e90a
Joseph Chappell <jdchappe@gmail.com>
2014-03-28 18:51:10 -0500

    Fix gulpfile to compile CoffeeScript and concat in single task. Also corrects destination for maker-console.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,13 @@
 var gulp = require('gulp'),
 gutil = require('gulp-util');
 
-//saves production files to ./public/
-
-//CoffeeScript Complier in .public/javascript/
+//compiles and concats CoffeesScript to maker-console.js in root directory
 var coffee = require('gulp-coffee');
-
-gulp.task('coffee', function(){
-  gulp.src('./lib/**/*.coffee')
-    .pipe(coffee({bare:true}).on('error', gutil.log))
-    .pipe(gulp.dest('./public/javascript'));
-});
-
-//concat production JS files in ./production/not_minified/
 var concatJS = require('gulp-concat');
 
-gulp.task('concatjs', function(){
-  gulp.src('./public/javascript/**/*.js')
-    .pipe(concatJS('all.js'))
-    .pipe(gulp.dest('./production/not_minified/'));
+gulp.task('concat-coffee', function(){
+  gulp.src('./lib/**/*.js.coffee')
+    .pipe(coffee({bare:true}).on('error', gutil.log))
+    .pipe(concatJS('maker-console.js'))
+    .pipe(gulp.dest('.'));
 });
