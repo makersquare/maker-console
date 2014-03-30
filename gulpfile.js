d181dca062fcb8cec1628c4b4948395edddd7154
Joseph Chappell <jdchappe@gmail.com>
2014-03-30 02:40:07 -0500

    Add app file and correct file order for gulp build-js task Also add minified output to same task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
 // Gulp.js for building production files
 // Install npm install gulp -g to execute gulp <task> in console
 var gulp = require('gulp'),
-gutil = require('gulp-util');
+gutil = require('gulp-util'),
+rename = require('gulp-rename');
 
-//compiles and concats CoffeesScript to maker-console.js in root directory
-var coffee = require('gulp-coffee');
-var concatJS = require('gulp-concat');
+//compiles and concats CoffeesScript to maker-console.js and in root directory
+// Also adds minified version maker-console.min.js
+var coffee = require('gulp-coffee'),
+concatJS = require('gulp-concat'),
+uglify = require('gulp-uglify'),
+file_order = ['./lib/maker-console.coffee', './lib/util.coffee', './lib/models/*.coffee', './lib/views/*.coffee', './lib/console/.*coffee'];
 
-gulp.task('concat-coffee', function(){
-  gulp.src('./lib/**/*.js.coffee')
+gulp.task('build-js', function(){
+  gulp.src(file_order)
     .pipe(coffee({bare:true}).on('error', gutil.log))
     .pipe(concatJS('maker-console.js'))
+    .pipe(gulp.dest('.'))
+    .pipe(uglify())
+    .pipe(rename('maker-console.min.js'))
     .pipe(gulp.dest('.'));
 });
